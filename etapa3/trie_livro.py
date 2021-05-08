1eb3be144e85af87f23b4515c045154d25abd9b9
Marthyna Weber <marthynaluiza@gmail.com>
2021-05-08 19:37:41 -0300

    classe Trie para Livros

---
+++ etapa3/trie_livro.py
@@ -0,0 +1,53 @@
+class nodoTrie_livro():
+    def __init__(self):
+        self.filhos = [None]*26
+        self.livro = ''
+        # fimPalavra é True se o nodo representa o fim de uma palavra
+        self.fimPalavra = False
+
+class TrieLivro:
+    def __init__(self):
+        self.raiz = self.getNode()
+  
+    def getNode(self):      
+        # Retorna novo nodo trie (inicializado para NULL)
+        return nodoTrie_livro()
+  
+    def charToIndex(self,ch):    
+        # Converte o char-chave atual em índice
+        # só usa 'a'-'z' em minúsculo          
+        return ord(ch)-ord('a')  
+  
+    def insere(self,chave,livro):          
+        # Se chave não está na árvore, inserte
+        # Se chave é prefixo de um nodo trie, marca como folha
+        atual = self.raiz
+        n = len(chave)
+        for nivel in range(n):
+            indice = self.charToIndex(chave[nivel])
+  
+            # se char atual não está presente
+            if not atual.filhos[indice]:
+                atual.filhos[indice] = self.getNode()
+            atual = atual.filhos[indice]
+  
+        # marca último nodo como folha e preenche seu atributo livro
+        atual.fimPalavra = True
+        atual.livro = livro
+  
+    def busca(self, chave):          
+        # Busca chave na trie
+        # Retorna true se a chave está presente, senão false
+        atual = self.raiz
+        n = len(chave)
+        for nivel in range(n):
+            indice = self.charToIndex(chave[nivel])
+            if not atual.filhos[indice]:
+                return False
+            atual = atual.filhos[indice]
+  
+        if atual != None and atual.fimPalavra:
+            return atual.livro
+        else:
+            return None
+  
